f5607e033d44ee637336ed1e7b7eaead5a414acd
saadeh <wilson.saadeh@gmail.com>
2020-06-08 17:12:40 -0300

    Inclusao categoria e produto
    Inclusao categoria e produto

--- MVC/CursoMVC/CursoMVC/Models/Context.cs
+++ MVC/CursoMVC/CursoMVC/Models/Context.cs
@@ -9,9 +9,10 @@ namespace CursoMVC.Models
     public class Context : DbContext 
     {
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+            optionsBuilder.UseSqlServer(@"Server=TMDC1DB02\BIZAGIDEV;Database=Cursomvc;User ID=sabizagi; Password=Bpm@2014; Trusted_Connection=False;");
 
         }
     }
